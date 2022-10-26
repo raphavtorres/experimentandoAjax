f0fd8b9d132412c5c446dc3426ebbb759f1be766
Raphael Torres <110772451+raphavtorres@users.noreply.github.com>
2022-10-26 09:02:04 -0300

    Add files via upload

---
+++ ajaxPW/request.php
@@ -0,0 +1,15 @@
+<?php
+
+    $nome = $_GET["nome"];
+/*     if($nome == "raphael"){
+        echo "usuario logado";
+    } else if ($nome == "henrique") {
+        echo "Você é muito guei man, se mat@";
+    } 
+    else {
+        echo "Você não é nada";
+    } */
+
+    echo $nome; //resposta do servidor
+
+?>
